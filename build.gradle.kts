85c1f559b199fdc220663543d124a0f72c2795cd
TheAlgorithm476 <thealgorithm476@gmail.com>
2022-12-04 21:19:57 +0100

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("java")
+    id("signing")
+    id("maven-publish")
+}
+
+group = "me.thealgorithm476"
+version = "2.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group as String
+            artifactId = project.name
+            version = project.version as String
+
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "octanrepo"
+            url = uri("https://maven.octandevelopment.com/releases")
+            credentials(PasswordCredentials::class)
+
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["maven"])
+}
