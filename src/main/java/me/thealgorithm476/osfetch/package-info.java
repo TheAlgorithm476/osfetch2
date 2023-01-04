5b479f5f0b1001f4cfa576a11048e136e5d8125e
TheAlgorithm476 <thealgorithm476@gmail.com>
2023-01-04 22:14:28 +0100

    [2.0.0] Add package-info.java

---
+++ src/main/java/me/thealgorithm476/osfetch/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package containing all code related to OsFetch, a simple tool to determine the JVM's underlying Operating System and Architecture
+ * @since 2.0.0
+ */
+package me.thealgorithm476.osfetch;
